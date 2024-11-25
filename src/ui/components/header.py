4168ec3b8d02b6c09c56e9464f3844870cf591c2
BlazzByte <nrwrntemporal2@gmail.com>
2024-11-24 21:29:10 -0400

    Feature: Update Paddings

--- src/ui/components/header.py
+++ src/ui/components/header.py
@@ -3,9 +3,10 @@ import streamlit as st
 def header():
     # Add a title
     st.title('Learn Faster with Learnicity:books:')
+    st.locale
     st.markdown(
         '<style>h1{color: orange; text-align: center;}</style>', unsafe_allow_html=True)
-    if 'input_text' not in st.session_state:
+    if 'input_text' not in st.session_state and 'file_content' not in st.session_state:
         st.subheader(
             'Turn any topic into flashcards, quizzes, and personalized resources to master it effortlessly.')
         st.markdown(
