48d05fc38b09d15213b86f3b7170c90fe3b5da66
BlazzByte <nrwrntemporal2@gmail.com>
2024-11-24 21:32:04 -0400

    Fix: Paddings from header

--- src/ui/components/header.py
+++ src/ui/components/header.py
@@ -3,11 +3,11 @@ import streamlit as st
 def header():
     # Add a title
     st.title('Learn Faster with Learnicity:books:')
-    st.locale
     st.markdown(
         '<style>h1{color: orange; text-align: center;}</style>', unsafe_allow_html=True)
     if 'input_text' not in st.session_state and 'file_content' not in st.session_state:
         st.subheader(
             'Turn any topic into flashcards, quizzes, and personalized resources to master it effortlessly.')
-        st.markdown(
-            '<style>h3{text-align: center;}</style>', unsafe_allow_html=True)
+        st.markdown('<style>h3{text-align: center;}</style>', unsafe_allow_html=True)
+    else:
+        st.markdown("""<style>.ea3mdgi5{padding: 2rem 5rem;}</style>""", unsafe_allow_html=True)
